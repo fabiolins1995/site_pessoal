f1a7cb59d9ff269101d5a31bc529195ea38c6fa5
fabiolins1995 <fabio.orlandini@yahoo.com.br>
2021-08-26 12:00:41 -0300

    Inplementação do menu de hamburguer

--- js/main.js
+++ js/main.js
@@ -48,3 +48,24 @@ if(target.length) {
         animeScroll();
     })
 }
+
+/*hamburguer menu*/
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
